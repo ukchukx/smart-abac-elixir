b3d942049adfbd57a3bdc928475306cf585bdea4
Uk Chukundah <ukchukundah@gmail.com>
2024-04-22 00:48:54 +0200

    Configure line length

--- .formatter.exs
+++ .formatter.exs
@@ -8,5 +8,6 @@
 
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  line_length: 120
 ]
